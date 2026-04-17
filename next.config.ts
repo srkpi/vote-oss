e6400d6090199cd5031e6ef4862b971f7289c8c5
Bielikov Maksym <40457162+Maximax67@users.noreply.github.com>
2026-04-17 14:39:11 +0300

    fix: relax CORP and COEP policy (#86)

--- next.config.ts
+++ next.config.ts
@@ -49,15 +49,7 @@ const nextConfig: NextConfig = {
             value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
           },
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
             value: 'same-origin',
           },
         ],
